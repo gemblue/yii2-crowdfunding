0a5e939af9267f4d1de0e0b1c61740507c8f5c5a
oriza <ahmadoriza@gmail.com>
2020-05-31 10:13:54 +0700

    Modify layouts

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -24,56 +24,37 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
+
 <?php $this->beginBody() ?>
 
-<div class="wrap">
-    <?php
-    NavBar::begin([
-        'brandLabel' => Yii::$app->name,
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
-        ],
-    ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post')
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link logout']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
-        ],
-    ]);
-    NavBar::end();
-    ?>
+<div class="row no-pad">
+    <div class="col-md-2">
+        <div id="sidebar">
+            <h3>CrowdFunding</h3>
 
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
+            <ul>
+                <li><a href="#"><i class="las la-home"></i>&nbsp;&nbsp;Dashboard</a></li>
+                <li><a href="#"><i class="lar la-user"></i>&nbsp;&nbsp;Users</a></li>
+                <li><a href="#"><i class="las la-user-tag"></i>&nbsp;&nbsp;Roles</a></li>
+                <li><a href="#"><i class="lab la-gratipay"></i>&nbsp;&nbsp;Campaign</a></li>
+                <li><a href="#"><i class="las la-tag"></i>&nbsp;&nbsp;Label</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="col-md-10">
+        <div id="content">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+
+        <footer>
+            <?= Yii::powered() ?>
+        </footer>
     </div>
 </div>
-
-<footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
-</footer>
 
 <?php $this->endBody() ?>
 </body>
