2311d0b8ca58695307652974b837e77b979c16bc
oriza <ahmadoriza@gmail.com>
2020-06-12 18:34:48 +0700

    Update frontend link

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -31,25 +31,25 @@ use yii\helpers\Url;
     <!-- Navigation -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
-            <a class="navbar-brand" href="#">CrowdFunding</a>
+            <a class="navbar-brand" href="<?php echo Url::home();?>">CrowdFunding</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home
+                        <a class="nav-link" href="<?php echo Url::home();?>">Home
                         <span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Tentang</a>
+                        <a class="nav-link" href="<?php echo Url::base();?>/site/about">Tentang</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Donasi</a>
+                        <a class="nav-link" href="<?php echo Url::base();?>/site/donate">Donasi</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Kontak</a>
+                        <a class="nav-link" href="<?php echo Url::base();?>/site/contact">Kontak</a>
                     </li>
 
                     <?php if (Yii::$app->user->isGuest) : ?>
