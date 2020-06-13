2021233178509024cf8d90f9c4488b1fb43cc807
oriza <ahmadoriza@gmail.com>
2020-06-13 23:03:58 +0700

    Create new page, donate

---
+++ views/site/donate.php
@@ -0,0 +1,23 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
+
+        <div class="mt-5 mb-5">
+            <h1>Cara Donasi</h1>
+
+            <p>Collaboratively whiteboard prospective benefits rather than stand-alone processes. Synergistically generate revolutionary leadership skills after viral strategic theme areas. Globally synergize user friendly partnerships whereas sticky resources. Dramatically cultivate plug-and-play e-tailers via cooperative services. Quickly restore backward-compatible applications with backward-compatible action items.</p>
+
+            <p>Proactively plagiarize collaborative "outside the box" thinking vis-a-vis top-line models. Authoritatively innovate impactful convergence for optimal convergence. Dynamically strategize installed base channels with mission-critical expertise. Objectively streamline open-source ROI rather than efficient services. Phosfluorescently deploy pandemic paradigms through cooperative architectures.</p>
+
+            <p>Phosfluorescently parallel task value-added manufactured products via ethical bandwidth. Intrinsicly mesh enterprise-wide methodologies vis-a-vis customized benefits. Credibly fabricate out-of-the-box process improvements via synergistic quality vectors. Holisticly evisculate maintainable web-readiness rather than flexible information. Energistically enable vertical growth strategies whereas functionalized metrics.</p>
+        </div>
+    </div>
+</div>
