395b85dd6853b53018504aeaaaa510783bd746f7
oriza <ahmadoriza@gmail.com>
2020-06-13 22:56:36 +0700

    Fix contact layout

--- views/site/contact.php
+++ views/site/contact.php
@@ -11,35 +11,35 @@ use yii\captcha\Captcha;
 $this->title = 'Contact';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row">
+    <div class="col-md-6 offset-md-3">
 
-    <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
+        <div class="mt-5 mb-5">
+            <h1><?= Html::encode($this->title) ?></h1>
 
-        <div class="alert alert-success">
-            Thank you for contacting us. We will respond to you as soon as possible.
-        </div>
+            <?php if (Yii::$app->session->hasFlash('contactFormSubmitted')): ?>
 
-        <p>
-            Note that if you turn on the Yii debugger, you should be able
-            to view the mail message on the mail panel of the debugger.
-            <?php if (Yii::$app->mailer->useFileTransport): ?>
-                Because the application is in development mode, the email is not sent but saved as
-                a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
-                Please configure the <code>useFileTransport</code> property of the <code>mail</code>
-                application component to be false to enable email sending.
-            <?php endif; ?>
-        </p>
+                <div class="alert alert-success">
+                    Thank you for contacting us. We will respond to you as soon as possible.
+                </div>
 
-    <?php else: ?>
+                <p>
+                    Note that if you turn on the Yii debugger, you should be able
+                    to view the mail message on the mail panel of the debugger.
+                    <?php if (Yii::$app->mailer->useFileTransport): ?>
+                        Because the application is in development mode, the email is not sent but saved as
+                        a file under <code><?= Yii::getAlias(Yii::$app->mailer->fileTransportPath) ?></code>.
+                        Please configure the <code>useFileTransport</code> property of the <code>mail</code>
+                        application component to be false to enable email sending.
+                    <?php endif; ?>
+                </p>
 
-        <p>
-            If you have business inquiries or other questions, please fill out the following form to contact us.
-            Thank you.
-        </p>
+            <?php else: ?>
 
-        <div class="row">
-            <div class="col-lg-5">
+                <p>
+                    If you have business inquiries or other questions, please fill out the following form to contact us.
+                    Thank you.
+                </p>
 
                 <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
@@ -60,9 +60,8 @@ $this->params['breadcrumbs'][] = $this->title;
                     </div>
 
                 <?php ActiveForm::end(); ?>
-
-            </div>
+                
+            <?php endif; ?>
         </div>
-
-    <?php endif; ?>
+    </div>
 </div>
