52d5274b33b3cc44f7e6d637aad8213cdf9a6715
oriza <ahmadoriza@gmail.com>
2020-06-13 23:03:45 +0700

    Update about template

--- views/site/about.php
+++ views/site/about.php
@@ -11,12 +11,14 @@ $this->params['breadcrumbs'][] = $this->title;
     <div class="col-md-6 offset-md-3">
 
         <div class="mt-5 mb-5">
-            <h1><?= Html::encode($this->title) ?></h1>
+            <h1>Tentang Kami</h1>
 
-            <p>
-                This is the About page. You may modify the following file to customize its content:
-            </p>
-        
+            <p>Holisticly seize mission-critical core competencies through emerging web services. Intrinsicly foster one-to-one relationships for revolutionary customer service. Professionally extend go forward strategic theme areas after multidisciplinary resources. Dynamically expedite turnkey customer service rather than premier core competencies. Conveniently disintermediate high standards in e-services through one-to-one services.</p>
+
+            <p>Proactively conceptualize enterprise-wide infrastructures and top-line e-tailers. Authoritatively generate accurate information with client-based scenarios. Phosfluorescently brand vertical intellectual capital without cutting-edge e-commerce. Monotonectally target multimedia based processes vis-a-vis turnkey interfaces. Competently fashion effective information with top-line communities.</p>
+
+            <p>Quickly envisioneer backend quality vectors vis-a-vis interoperable outsourcing. Dramatically disseminate bricks-and-clicks leadership for innovative communities. Completely impact leading-edge supply chains without mission-critical.</p>
+            
         </div>
     </div>
 </div>
