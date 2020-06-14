dfcea95ef418750bdbb70c87b0392b88819aafd2
oriza <ahmadoriza@gmail.com>
2020-06-14 21:49:15 +0700

    Update views

--- modules/admin/views/roles/_search.php
+++ modules/admin/views/roles/_search.php
@@ -28,7 +28,7 @@ use yii\widgets\ActiveForm;
     <?php // echo $form->field($model, 'created_at') ?>
 
     <?php // echo $form->field($model, 'updated_at') ?>
-
+    
     <div class="form-group">
         <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
